24f5c45c62a3b614b8aab71a2f8703780be61714
kimgwanjung <qwe2846@naver.com>
2023-04-12 15:44:32 +0900

    HI HI

--- Algorithm_Study/src/Kopo_Algorithm/Hello.java
+++ Algorithm_Study/src/Kopo_Algorithm/Hello.java
@@ -2,6 +2,6 @@ package Kopo_Algorithm;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HI");    
+        System.out.println("HI hi");    
     }
 }
